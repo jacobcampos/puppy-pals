540323c2b52a60e9228345213a9bcea95f9571c8
Jacob Campos <3jacoblcampos@gmail.com>
2023-06-29 15:31:21 -0500

    added puppylist data

--- vite-puppy-pals/src/App.jsx
+++ vite-puppy-pals/src/App.jsx
@@ -1,15 +1,24 @@
+import { useState } from 'react'
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
 
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppylist: ", puppyList);
 
   return (
     <>
-      <div>
-
+      <div className="App">
+        { 
+           puppies.map((puppy) => {
+              return <p key ={puppy.name}>{puppy.name}</p>
+             })
+        }
       </div>
     </>
-  )
+  );
 }
 
 export default App
